c57712882dc2af3396185b8af836b02d43e0ca88
yyama <dummy@ne.jp>
2018-09-29 15:52:41 +0900

    「追加」ボタンを機能するように修正した。

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <h1>Todo List</h1>
-        <input type="text">
-        <input type="button" value="追加">
+        <input type="text" v-model="newTask">
+        <input type="button" value="追加" v-on:click="addTask">
         <div class="list">
             <ul>
                 <li v-for="item in list" :key="item.id">{{ item.value }}</li>
@@ -19,9 +19,19 @@ export default {
                { id:1, value: "たまご買う" },
                { id:2, value: "図書館に本を返す" },
                { id:3, value: "宅急便を受け取る" }
-            ]
+            ],
+            newTask: "",
+            nextId: 4
         }
-    } 
+    } ,
+    methods : {
+        addTask() {
+            this.list.push({
+                id: this.nextId++,
+                value: this.newTask
+            })
+        }
+    }
 }
 </script>
 
